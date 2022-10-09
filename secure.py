8b32959d8fbfe451874864697af911859d003a85
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-10-09 16:19:50 -0700

    adding

---
+++ secure.py
@@ -0,0 +1,13 @@
+#Take two inputs from user(int/float)
+#print the greatest number
+
+
+user1 = int(input("user1 number:"))
+user2 = float(input("user2 number:"))
+
+
+if user1 > user2:
+     print(" user1 is greatest")
+
+elif user1 < user2:
+    print("lowest")     
